93291bb24cf9d9f6593b5119fa42839f192923ae
JoselineArianaMarcial <marcialariana8@gmail.com>
2024-10-28 15:41:31 -0600

    Confirmacion 1 OK Marcial
    Datos desarrollo: Joseline Marcial

---
+++ Sesion34MarcialED_pilas/src/sesion34marcialed_pilas/Sesion34MarcialED_pilas.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sesion34marcialed_pilas;
+
+import java.util.Stack;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author LISUACM
+ */
+public class Sesion34MarcialED_pilas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+        Stack<Integer> pilaMarcial= new Stack<>();
+pilaMarcial.push(100);
+pilaMarcial.push(150);
+pilaMarcial.push(230);
+pilaMarcial.push(10);
+pilaMarcial.push(160);
+
+        JOptionPane.showMessageDialog(null, "es:\n"+ pilaMarcial);
+        
+    }
+    
+}
